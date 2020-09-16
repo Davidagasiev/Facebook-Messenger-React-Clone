cc6a702d47113f387e388c0bd84acdd3005f1e9b
David <davidagasiev2005@gmail.com>
2020-09-16 20:57:01 +0400

    Post Adding Ready

--- src/features/Messages/Message/Message.jsx
+++ src/features/Messages/Message/Message.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useSelector } from "react-redux";
 
 import "./Message.scss";
+import { UsersSelector } from "../../Users/UsersSlice";
 
 export default function Message(props) {
+    const message = props.message;
+    const users = useSelector(UsersSelector);
+
+    const [thisUser, setThisUser] = useState({});
+
+    useEffect(() => {
+        if(users.length !== 0){
+            setThisUser(users.find(user => user.uid === message.uid))
+        }
+    }, [users])
+
     return (
         <div>
             <div>
