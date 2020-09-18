e0bd132ca82ea4f6bc832a8757ed1f1d87979553
David <davidagasiev2005@gmail.com>
2020-09-18 10:45:22 +0400

    To Bottom

--- src/features/Main/Main.jsx
+++ src/features/Main/Main.jsx
@@ -1,15 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 
 import "./Main.scss";
 
-import { db, auth } from "../../app/firebase";
+import { db } from "../../app/firebase";
 import {MessageSelector, updateMessages} from "../Messages/MessagesSlice";
 
 import MessageList from "../Messages/MessageList/MessageList.jsx";
 import MessageAdding from "../Messages/MessageAdding/MessageAdding";
 import Navbar from "../Navbar/Navbar.jsx";
 import Welcome from '../Welcome/Welcome';
+
+import { IconButton } from '@material-ui/core';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 
 export default function Main() {
 
@@ -23,6 +26,26 @@ export default function Main() {
           })
       }, [])
 
+// For Got To bottom button. 
+      const [showToBottom, setShowToButton] = useState(true);
+
+      const listenToScroll = () => {
+        if(window.innerHeight + 400 < document.body.clientHeight - window.pageYOffset){
+          setShowToButton(true);
+        }else{
+          setShowToButton(false);
+        }
+      }
+      
+      useEffect(() => {
+        window.addEventListener('scroll', listenToScroll)
+
+        return () => {
+          window.removeEventListener('scroll', listenToScroll);
+        }
+      })
+
+
     return (
         <>
           <Navbar />
@@ -30,6 +53,16 @@ export default function Main() {
               <Welcome style={{marginTop: "100px"}}/>
               <MessageList messages={messages} />
           </div>
+          { showToBottom ?
+                <div className="toBottom">
+                    <div>
+                        <IconButton onClick={() => window.scrollTo(0, document.body.clientHeight)} size="medium">
+                            <ArrowDownwardIcon fontSize="inherit" />
+                        </IconButton> 
+                    </div>
+                </div>
+                : ""
+            }
           <MessageAdding />
         </>
     )
--- src/features/Messages/MessageList/MessageList.jsx
+++ src/features/Messages/MessageList/MessageList.jsx
@@ -4,16 +4,19 @@ import Message from '../Message/Message.jsx'
 import "./MessageList.scss";
 import { mergeSort } from "./Functions";
 
+
 export default function MessageList(props) {
 
     const messages = mergeSort(props.messages);
 
     return (
+        <>
         <div className="MessageList">
             {messages.map(message => (
                 <Message key={message.id} message={message}/>
             ))
             }
         </div>
+        </>
     )
 }
